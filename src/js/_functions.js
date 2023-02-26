0f846d4a9a43ca6a0096983ef852a157048abe65
alexdolg1967 <alexdolg1967@mail.ru>
2023-02-26 12:04:08 +0300

    first commit

---
+++ src/js/_functions.js
@@ -0,0 +1,9 @@
+
+// Определение операционной системы на мобильных
+import { mobileCheck } from "./functions/mobile-check";
+// console.log(mobileCheck());
+
+// Реализация табов
+// import GraphTabs from "graph-tabs";
+// const tabs = new GraphTabs("sign");
+
